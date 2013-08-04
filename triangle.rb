924f52396affee890d0439449078c3fd9f08f5e0
Pieter Kuijpers <pieter@pi-q.nl>
2013-08-04 21:04:27 +0200

    To 171

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  fail TriangleError if a == 0 and b == 0 and c == 0
+  fail TriangleError if a < 0 or b < 0 or c < 0
+
+  # No side can be longer than the other two combined
+  fail TriangleError if (a >= b + c) or (b >= a + c) or (c >= a + b)
+
+  return :equilateral if (a == b) and (b == c)
+  return :scalene if (a != b) and (b != c) and (a != c)
+  :isosceles
 end
 
 # Error class used in part 2.  No need to change this code.
